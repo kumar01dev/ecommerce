a9e6e5714f21df8fc056bdd95649c462bac4249a
Kumar Sourav <Kumarmahto39@gmail.com>
2025-04-04 15:56:27 +0530

    change cors from backend

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://ecommerce-frontend-two-delta.vercel.app', // Allow only your frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
